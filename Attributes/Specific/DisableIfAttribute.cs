fec14152f63d2c9b894f666aef26c7e34bee592c
PE2D <nsgbcz@gmail.com>
2022-04-03 11:50:36 +0300

    AllowMultiple for several attributes

--- Attributes/Specific/DisableIfAttribute.cs
+++ Attributes/Specific/DisableIfAttribute.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace Fusumity.Attributes.Specific
 {
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
 	public class DisableIfAttribute : GenericDrawerAttribute
 	{
 		public string boolPath;
