a35d45d7d894213c086ba535039cd4a9abbb3f27
Vlod Shvetsov <nsgbcz@gmail.com>
2023-02-16 12:10:20 +0300

    Update show/hide attributes

--- Attributes/Specific/HideIfAttribute.cs
+++ Attributes/Specific/HideIfAttribute.cs
@@ -5,11 +5,15 @@ namespace Fusumity.Attributes.Specific
 	[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
 	public class HideIfAttribute : FusumityDrawerAttribute
 	{
-		public string boolPath;
+		public string checkPath;
+		public object[] equalsAny;
 
-		public HideIfAttribute(string boolPath)
+		public HideIfAttribute(string checkPath, params object[] equalsAny)
 		{
-			this.boolPath = boolPath;
+			equalsAny ??= new object[] { null };
+
+			this.checkPath = checkPath;
+			this.equalsAny = equalsAny;
 		}
 	}
 }
--- Attributes/Specific/ShowIfAttribute.cs
+++ Attributes/Specific/ShowIfAttribute.cs
@@ -5,11 +5,15 @@ namespace Fusumity.Attributes.Specific
 	[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
 	public class ShowIfAttribute : FusumityDrawerAttribute
 	{
-		public string boolPath;
+		public string checkPath;
+		public object[] equalsAny;
 
-		public ShowIfAttribute(string boolPath)
+		public ShowIfAttribute(string checkPath, params object[] equalsAny)
 		{
-			this.boolPath = boolPath;
+			equalsAny ??= new object[] { null };
+
+			this.checkPath = checkPath;
+			this.equalsAny = equalsAny;
 		}
 	}
 }
