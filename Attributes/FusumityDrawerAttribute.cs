66bedb6980d362032486a63725e0488b23894568
Vlod Shvetsov <nsgbcz@gmail.com>
2023-07-30 11:30:54 +0300

    AllowMultiple for FusumityDrawerAttribute

--- Attributes/FusumityDrawerAttribute.cs
+++ Attributes/FusumityDrawerAttribute.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 namespace Fusumity.Attributes
 {
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
 	public class FusumityDrawerAttribute : PropertyAttribute {}
 
 	public interface IFusumitySerializable {}
