42e056ef1d7ba835d69964c600e4f928b822b541
Vlod Shvetsov <nsgbcz@gmail.com>
2024-01-13 10:18:00 +0300

    Update ClipboardExt

--- Editor/Extensions/ClipboardExt.cs
+++ Editor/Extensions/ClipboardExt.cs
@@ -10,14 +10,19 @@ namespace Fusumity.Editor.Extensions
 
 		public static void CopyManagedReferenceValue(this SerializedProperty target)
 		{
+			target.CopyManagedReferenceValue(out _source);
+		}
+
+		public static void CopyManagedReferenceValue(this SerializedProperty target, out object source)
+		{
 #if UNITY_2022_3_OR_NEWER
 			if (target.managedReferenceValue == null)
 			{
-				_source = null;
+				source = null;
 				return;
 			}
-			_source = Activator.CreateInstance(target.managedReferenceValue.GetType());
-			EditorUtility.CopySerializedManagedFieldsOnly(target.managedReferenceValue, _source);
+			source = Activator.CreateInstance(target.managedReferenceValue.GetType());
+			EditorUtility.CopySerializedManagedFieldsOnly(target.managedReferenceValue, source);
 #else
 			var type = target.GetManagedReferenceType();
 			_source = Activator.CreateInstance(type);
@@ -28,13 +33,18 @@ namespace Fusumity.Editor.Extensions
 
 		public static void PasteManagedReferenceValue(this SerializedProperty target)
 		{
-			if (_source == null)
+			target.PasteManagedReferenceValue(_source);
+		}
+
+		public static void PasteManagedReferenceValue(this SerializedProperty target, object source)
+		{
+			if (source == null)
 			{
 				target.managedReferenceValue = null;
 				return;
 			}
-			var value = Activator.CreateInstance(_source.GetType());
-			EditorUtility.CopySerializedManagedFieldsOnly(_source, value);
+			var value = Activator.CreateInstance(source.GetType());
+			EditorUtility.CopySerializedManagedFieldsOnly(source, value);
 			try
 			{
 				target.managedReferenceValue = value;
