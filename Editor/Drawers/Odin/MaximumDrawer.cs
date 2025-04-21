f6c2bb6cc1632fa2495efb8bf22b8b0129c6c2ae
Vladislav Shvetsov <vlad.shvetsov@winzardy.com>
2025-04-21 12:39:37 +0300

    Fix MaximumDrawer

--- Editor/Drawers/Odin/MaximumDrawer.cs
+++ Editor/Drawers/Odin/MaximumDrawer.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 namespace Fusumity.Editor.Drawers
 {
+	[DrawerPriority(0, 9000, 0)]
 	public sealed class MaximumDrawer<T> : OdinAttributeDrawer<MaximumAttribute, T>
 		where T : struct
 	{
