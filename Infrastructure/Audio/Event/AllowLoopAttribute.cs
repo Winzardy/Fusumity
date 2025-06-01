929d118f533388926330581b7d9749750ff80be5
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2025-06-01 16:24:13 +0300

    [*] Minor updated Audio system #2

---
+++ Infrastructure/Audio/Event/AllowLoopAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Audio
+{
+	/// <summary>
+	/// Добавляет или убирает возможно настраивать зацикленное воспроизведение в <see cref="AudioEventRequest"/>
+	/// </summary>
+	public class AllowLoopAttribute : Attribute
+	{
+		public bool Loop { get; set; }
+
+		public AllowLoopAttribute(bool loop)
+		{
+			Loop = loop;
+		}
+	}
+}
