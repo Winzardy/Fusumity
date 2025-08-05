16c6d333c17f70daed5f4126f843dbe0f208d7c2
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2025-08-05 12:47:33 +0300

    [TASK-2088] [*] Minor fixed DefeatWindow/VictoryWindow animators

--- Infrastructure/UI/Windows/DefaultWindowAnimator.cs
+++ Infrastructure/UI/Windows/DefaultWindowAnimator.cs
@@ -8,34 +8,40 @@ namespace UI.Windows
 		private const float CLOSING_TIME = 0.3f;
 
 		protected override void OnCreateOpeningSequence(ref Sequence sequence)
+			=> CreateOpeningSequence(_layout, ref sequence);
+
+		protected override void OnCreateClosingSequence(ref Sequence sequence)
+			=> CreateClosingSequence(_layout, ref sequence);
+
+		public static void CreateOpeningSequence(UIBaseContainerLayout layout, ref Sequence sequence)
 		{
 			sequence ??= DOTween.Sequence();
 
-			if (_layout.canvasGroup)
+			if (layout.canvasGroup)
 			{
-				sequence.Join(_layout.canvasGroup
+				sequence.Join(layout.canvasGroup
 				   .DOFade(1, OPENING_TIME)
 				   .From(0));
 			}
 
-			sequence.Join(_layout.container
+			sequence.Join(layout.container
 			   .DOScale(1, OPENING_TIME)
 			   .From(1.1f)
 			   .SetEase(Ease.OutBack));
 		}
 
-		protected override void OnCreateClosingSequence(ref Sequence sequence)
+		public static void CreateClosingSequence(UIBaseContainerLayout layout, ref Sequence sequence)
 		{
 			sequence ??= DOTween.Sequence();
 
-			if (_layout.canvasGroup)
+			if (layout.canvasGroup)
 			{
-				sequence.Join(_layout.canvasGroup
+				sequence.Join(layout.canvasGroup
 				   .DOFade(0, CLOSING_TIME * 0.7f)
 				   .From(1));
 			}
 
-			sequence.Join(_layout.container
+			sequence.Join(layout.container
 			   .DOScale(0.9f, CLOSING_TIME)
 			   .From(1)
 			   .SetEase(Ease.OutCubic));
