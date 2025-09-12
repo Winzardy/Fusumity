176b5c3f21a51456c8590936e0cc255fee817c12
Vladislav Shvetsov <vlad.shvetsov@winzardy.com>
2025-09-12 16:46:56 +0300

    IDataMigrator. Add warning comment.

--- Infrastructure/Migration/IDataMigrator.cs
+++ Infrastructure/Migration/IDataMigrator.cs
@@ -1,8 +1,14 @@
 using System;
+using Fusumity.Attributes.Specific;
 using UnityEngine;
 
 namespace Submodules.Fusumity.Infrastructure.Migration
 {
+	/// <summary>
+	/// ВНИМАНИЕ!
+	/// `ISerializationCallbackReceiver` работает только для классов.
+	/// Не использовать для структур!
+	/// </summary>
 	[Obsolete("Наследника этого интерфейса нужно мигрировать и удалить наследование от этого интерфейса. Сам интерфейс не удалять!")]
 	public interface IDataMigrator : ISerializationCallbackReceiver
 	{
