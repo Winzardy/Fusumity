5b0bdc6ef76fcdc73c9da89451325e66fff56fe4
Vladislav Shvetsov <vlad.shvetsov@winzardy.com>
2025-09-19 14:29:12 +0300

    Add EnumList<TEnum>

--- Collections/EnumCollections/EnumList.cs
+++ Collections/EnumCollections/EnumList.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Sapientia.Collections;
+using Sapientia.Data;
+using Sapientia.Extensions;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -19,6 +21,51 @@ namespace Fusumity.Collections
 	public class EnumList<TEnum, TValue> : BaseEnumList<TEnum, EnumValueEditableEnum<TEnum, TValue>>
 		where TEnum : unmanaged, Enum
 	{
+	}
+
+	[Serializable]
+	public class EnumList<TEnum> : BaseEnumList<TEnum, EnumValueEditableEnum<TEnum>>
+		where TEnum : unmanaged, Enum
+	{
+		public EnumMask<TEnum> Mask { get; private set; }
+
+		public override void OnAfterDeserialize()
+		{
+			base.OnAfterDeserialize();
+			Mask = GetEnumMask();
+		}
+
+		public static implicit operator EnumList<TEnum>(TEnum value)
+		{
+			return new EnumList<TEnum>()
+			{
+				values = new EnumValueEditableEnum<TEnum>[]
+				{
+					value
+				}
+			};
+		}
+
+		public static implicit operator EnumList<TEnum>(EnumMask<TEnum> mask)
+		{
+			var span = (Span<TEnum>)stackalloc TEnum[EnumValues<TEnum>.ENUM_LENGHT];
+			var count = 0;
+			foreach (var enumValue in EnumValues<TEnum>.VALUES)
+			{
+				if (mask.Has(enumValue))
+					span[count++] = enumValue;
+			}
+			var result = new EnumList<TEnum>()
+			{
+				values = new EnumValueEditableEnum<TEnum>[count],
+			};
+			for (var i = 0; i < count; i++)
+			{
+				result.values[i] = span[i];
+			}
+
+			return result;
+		}
 	}
 
 	[Serializable]
@@ -49,6 +96,16 @@ namespace Fusumity.Collections
 		public void Add(TEnumValue value)
 		{
 			ArrayExt.Add(ref values, value);
+		}
+
+		public EnumMask<TEnum> GetEnumMask()
+		{
+			var result = new EnumMask<TEnum>();
+			foreach (var value in values)
+			{
+				result.Add(value.EnumValue);
+			}
+			return result;
 		}
 
 		public virtual void OnBeforeSerialize()
