06d528eb8a03c65f3f67bc296cbf51f6117bd031
Valeriy Logvinov <valeriy.logvinov@winzardy.com>
2026-01-12 23:23:07 +0300

    [TASK-2448] Added SetValue extension for scrobjects.

--- Infrastructure/Content/ScriptableObjects/ContentEntryScriptableObject.Editing.cs
+++ Infrastructure/Content/ScriptableObjects/ContentEntryScriptableObject.Editing.cs
@@ -4,7 +4,23 @@
 
 	public static class ContentEntryScriptableObjectExtensions
 	{
-		public static void Edit<TValue>(this IContentEntryScriptableObject<TValue> scriptableObject,
+		public static void SetValue<TValue>(this IContentEntryScriptableObject<TValue> scriptableObject, in TValue value, bool save = true)
+		{
+			scriptableObject.EditValue = value;
+
+			if (!save)
+				return;
+
+#if UNITY_EDITOR
+			UnityEditor.EditorUtility.SetDirty((UnityObject)scriptableObject);
+			UnityEditor.AssetDatabase.SaveAssetIfDirty((UnityObject)scriptableObject);
+#endif
+		}
+
+		/// <summary>
+		/// Use that to change specific fields in value.
+		/// </summary>
+		public static void EditValue<TValue>(this IContentEntryScriptableObject<TValue> scriptableObject,
 			ContentEditing<TValue> editing,
 			bool save = true)
 		{
@@ -15,8 +31,8 @@
 				return;
 
 #if UNITY_EDITOR
-			UnityEditor.EditorUtility.SetDirty((UnityObject) scriptableObject);
-			UnityEditor.AssetDatabase.SaveAssetIfDirty((UnityObject) scriptableObject);
+			UnityEditor.EditorUtility.SetDirty((UnityObject)scriptableObject);
+			UnityEditor.AssetDatabase.SaveAssetIfDirty((UnityObject)scriptableObject);
 #endif
 		}
 	}
