c96a62c7f15bbd6c0a8d0156755433951c8aed1e
VLogvinov <lvn.formal@gmail.com>
2026-01-15 21:03:35 +0300

    [BUG-142] Removed scene management boot task, added utility for exclusive object activation.

---
+++ Utility/Other/ExclusiveActivator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fusumity.Utility
+{
+	public interface IExclusiveActivated
+	{
+		int Priority { get; }
+		void SetActive(bool active);
+	}
+
+	public class ExclusiveActivator : ExclusiveActivator<IExclusiveActivated> { }
+	public class ExclusiveActivator<T> where T : class, IExclusiveActivated
+	{
+		private List<T> _objects = new List<T>();
+
+		public T CurrentlyActive { get; private set; }
+
+		public event Action<T> CurrentlyActiveChanged;
+
+		public void Add(T obj)
+		{
+			_objects.Add(obj);
+			Reevaluate(obj);
+		}
+
+		public void Remove(T obj)
+		{
+			_objects.Remove(obj);
+			Reevaluate();
+		}
+
+		private void Reevaluate(T focused = null)
+		{
+			var highest = focused;
+
+			for (int i = 0; i < _objects.Count; i++)
+			{
+				var obj = _objects[i];
+
+				if (highest == null || highest.Priority < obj.Priority)
+					highest = obj;
+			}
+
+			if (CurrentlyActive == highest)
+				return;
+
+			CurrentlyActive?.SetActive(false);
+			CurrentlyActive = highest;
+			CurrentlyActive?.SetActive(true);
+
+			CurrentlyActiveChanged?.Invoke(CurrentlyActive);
+		}
+	}
+}
