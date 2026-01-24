2eb4aaee958f8a84ef1ae7764e4efc630d29c521
VLogvinov <lvn.formal@gmail.com>
2026-01-24 23:54:29 +0300

    ConstDropdownAttribute.

---
+++ Attributes/Odin/ConstDropdownAttribute.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Diagnostics;
+
+namespace Fusumity.Attributes.Odin
+{
+	[Conditional("UNITY_EDITOR")]
+	public class ConstDropdownAttribute : Attribute
+	{
+		public Type Type { get; }
+		public string TypeName { get; }
+		public bool PrettyPrint { get; } = true;
+
+		/// <summary>
+		/// Make sure that there is default (None) value to select.
+		/// </summary>
+		public bool EnsureDefaultValue { get; set; } = true;
+
+		public ConstDropdownAttribute(Type type)
+		{
+			this.Type = type;
+		}
+
+		public ConstDropdownAttribute(Type type, bool prettyPrint)
+		{
+			this.Type = type;
+			this.PrettyPrint = prettyPrint;
+		}
+
+		public ConstDropdownAttribute(string typeName)
+		{
+			this.TypeName = typeName;
+		}
+
+		public ConstDropdownAttribute(string typeName, bool prettyPrint)
+		{
+			this.TypeName = typeName;
+			this.PrettyPrint = prettyPrint;
+		}
+	}
+}
