b512615525942f02d43b5d56f3384ee459b01b31
Timur Nikolaev <timur.nikolaev@winzardy.com>
2026-02-09 18:50:56 +0500

    Add ParticleSystemPlayAudioEventTrigger

---
+++ Infrastructure/Audio/Triggers/Particles/ParticleSystemPlayAudioEventTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+	public sealed class ParticleSystemPlayAudioEventTrigger : AudioEventTrigger
+	{
+		[SerializeField] private ParticleSystem _particleSystem;
+
+		[AllowLoop(true)] public AudioEventRequest onPlay;
+
+		private AudioPlayback _playback;
+		private bool _wasPlaying;
+
+		private void OnEnable()
+		{
+			_wasPlaying = false;
+		}
+
+		private void Update()
+		{
+			if (_particleSystem == null || onPlay.IsEmpty)
+				return;
+
+			bool isPlaying = _particleSystem.isPlaying;
+
+			if (!_wasPlaying && isPlaying)
+			{
+				_playback = onPlay.Play(transform);
+			}
+
+			_wasPlaying = isPlaying;
+		}
+
+		private void OnDisable()
+		{
+			_playback?.Dispose();
+		}
+	}
+}
