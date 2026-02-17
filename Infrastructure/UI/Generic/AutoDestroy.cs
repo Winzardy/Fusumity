3459837d9e663d3078d7bca967e3ab5ba48168d1
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-02-18 00:06:18 +0300

    [TASK-2252] [*] Updated Window_Gacha_Pavilion (wip)

---
+++ Infrastructure/UI/Generic/AutoDestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+	/// <summary>
+	/// Авто-уничтожение на Awake
+	///
+	/// Компонент используется в основном для того, чтобы в определённых ситуациях, например при работе с template's,
+	/// не приходилось вручную отключать их.
+	/// </summary>
+	public class AutoDestroy : MonoBehaviour
+	{
+		private void Awake() => Destroy(gameObject);
+	}
+}
