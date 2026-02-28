13e96441ce033407f093d904c70a9654584c708f
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-02-28 17:13:05 +0300

    [TASK-2252] [*] Fixed precent view

--- Editor/AttributeProcessors/WeightableAttributeProcessor.cs
+++ Editor/AttributeProcessors/WeightableAttributeProcessor.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Reflection;
 using Fusumity.Attributes;
 using Sapientia;
+using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
 
 namespace Fusumity.Editor
 {
-	public class WeightableAttributeProcessor : OdinAttributeProcessor<IWeightable>
+	public abstract class BaseWeightableAttributeProcessor<T> : OdinAttributeProcessor<T>
 	{
 		public override void ProcessChildMemberAttributes(InspectorProperty parentProperty, MemberInfo member, List<Attribute> attributes)
 		{
@@ -17,9 +20,40 @@ namespace Fusumity.Editor
 			{
 				case "weight":
 					attributes.Add(new DarkCardBoxAttribute());
+
+					attributes.Add(new SuffixLabelAttribute("@WeightableAttributeProcessor.GetSuffixLabel($property)", true));
 					break;
 			}
 		}
+	}
 
+	public class WeightableAttributeProcessor : BaseWeightableAttributeProcessor<IWeightable>
+	{
+		private const string SPACE = "     ";
+
+		public static string GetSuffixLabel(InspectorProperty property)
+		{
+			if (property.ParentValueProperty.ParentValueProperty.ValueEntry.WeakSmartValue is ICollection collection)
+			{
+				var totalWeight = 0;
+				var currentWeight = property.ParentValueProperty.ValueEntry.WeakSmartValue is IWeightable current ? current.Weight : 0;
+				foreach (var obj in collection)
+				{
+					if (obj is IWeightable weightable)
+					{
+						totalWeight += weightable.Weight;
+					}
+				}
+
+				var percent = (float) currentWeight / totalWeight;
+				return percent.ToString("P4") + SPACE;
+			}
+
+			return "?%" + SPACE;
+		}
+	}
+
+	public class WeightableWithEvaluatorAttributeProcessor : BaseWeightableAttributeProcessor<IWeightableWithEvaluator>
+	{
 	}
 }
