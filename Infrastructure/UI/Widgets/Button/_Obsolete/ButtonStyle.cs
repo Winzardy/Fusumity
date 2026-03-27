c75612c7725523dd3bb024ed269de4af21268664
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-03-27 17:05:44 +0300

    [TASK-2217] [*] Minor updated after merge

--- Infrastructure/UI/Widgets/Button/_Obsolete/ButtonStyle.cs
+++ Infrastructure/UI/Widgets/Button/_Obsolete/ButtonStyle.cs
@@ -2,11 +2,12 @@
 {
 	public class UIButtonStyle
 	{
+		public const string DISABLED = "disabled"; // Когда кнопка скрывается
+
 		public const string DEFAULT = "default";
 		public const string FOCUS = "focus";
-		public const string DISABLED = "disabled";
 		public const string LOCKED = "locked";
-		public const string UNAVAILABLE = "unavailable";
+		public const string UNAVAILABLE = "unavailable"; // Серая кнопка!
 		public const string ADS = "ads";
 	}
 }
