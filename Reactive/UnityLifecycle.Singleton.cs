1915f08757e905e5a5a2aff2c0f627e9961aa20b
Konstantin Postoev <konstantin.postoev@gmail.com>
2026-07-24 21:11:26 +0700

    Init some unity tools via boottask

--- Reactive/UnityLifecycle.Singleton.cs
+++ Reactive/UnityLifecycle.Singleton.cs
@@ -7,8 +7,7 @@ namespace Fusumity.Reactive
 	{
 		private static UnityLifecycle _instance;
 
-		[RuntimeInitializeOnLoadMethod]
-		private static void Initialize()
+		public static void Initialize()
 		{
 			var go = new GameObject($"[{nameof(UnityLifecycle)}]");
 			_instance = go.AddComponent<UnityLifecycle>();
