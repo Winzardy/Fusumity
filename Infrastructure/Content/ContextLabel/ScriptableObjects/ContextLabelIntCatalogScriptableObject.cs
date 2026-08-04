ba62a1299d9c6a6a5220dbeebc9e9e02fff4a52b
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-08-04 11:39:12 +0300

    [+] Added ContentEntryConstantGenerator [*] Updated ContentScriptableObject

--- Infrastructure/Content/ContextLabel/ScriptableObjects/ContextLabelIntCatalogScriptableObject.cs
+++ Infrastructure/Content/ContextLabel/ScriptableObjects/ContextLabelIntCatalogScriptableObject.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
 using Content.ContextLabel;
 using UnityEngine;
 
 namespace Content.ScriptableObjects
 {
-	// [Constants]
-	// TODO: сделать генерацию констант через SO
-	// Хороший вопрос что тип данных может быть помечен Constants, а SO's можеть быть несколько... Не подумла об этом
 	[CreateAssetMenu(menuName = ContentMenuConstants.CREATE_MENU + "Misc/Label/Int Catalog", fileName = "ContextLabels_Int_New")]
 	public class ContextLabelIntCatalogScriptableObject : ContentEntryScriptableObject<ContextLabelCatalog<int>>
 	{
+		public override bool UseConstants { get => true; }
+
+		// У int-ключа иерархии нет, имя константы берём из лейбла — единственного
+		// человекочитаемого текста, который у записи есть
+		public override IEnumerable<ContentConstantEntry> EnumerateConstants()
+		{
+			foreach (var key in Value.GetKeys())
+				yield return new ContentConstantEntry(Value[key], key, summary: Value[key]);
+		}
 	}
 }
--- Infrastructure/Content/ContextLabel/ScriptableObjects/ContextLabelStringCatalogScriptableObject.cs
+++ Infrastructure/Content/ContextLabel/ScriptableObjects/ContextLabelStringCatalogScriptableObject.cs
@@ -1,13 +1,23 @@
+using System.Collections.Generic;
 using Content.ContextLabel;
 using UnityEngine;
 
 namespace Content.ScriptableObjects
 {
-	// [Constants]
-	// TODO: сделать генерацию констант через SO
-	// Хороший вопрос что тип данных может быть помечен Constants, а SO's можеть быть несколько... Не подумла об этом
 	[CreateAssetMenu(menuName = ContentMenuConstants.CREATE_MENU + "Misc/Label/String Catalog", fileName = "ContextLabels_String_New")]
 	public class ContextLabelStringCatalogScriptableObject : ContentEntryScriptableObject<ContextLabelCatalog<string>>
 	{
+		public override bool UseConstants { get => true; }
+
+		// Ключ несёт иерархию ("hair/00"): из него и имя константы, и группа-заголовок
+		public override IEnumerable<ContentConstantEntry> EnumerateConstants()
+		{
+			foreach (var key in Value.GetKeys())
+			{
+				var separatorIndex = key.LastIndexOf('/');
+				var group = separatorIndex > 0 ? key[..separatorIndex] : null;
+				yield return new ContentConstantEntry(key, key, group, Value[key]);
+			}
+		}
 	}
 }
