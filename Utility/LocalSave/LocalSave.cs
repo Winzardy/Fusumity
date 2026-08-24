98fa13328667051c622609275680c625c679272d
Konstantin Postoev <konstantin.postoev@gmail.com>
2026-08-24 16:05:21 +0700

    LocalSave saving

--- Utility/LocalSave/LocalSave.cs
+++ Utility/LocalSave/LocalSave.cs
@@ -5,11 +5,14 @@ namespace Fusumity.Utility
 {
 	public class LocalSave
 	{
-		public static void Save<T>(string key, T value)
+		public static void Save<T>(string key, T value, bool save = true)
 		{
 			var json = value.ToJson();
 
 			PlayerPrefs.SetString(key, json);
+
+			if (save)
+				PlayerPrefs.Save();
 		}
 
 		public static T Load<T>(string key, T defaultValue = default)
@@ -21,14 +24,24 @@ namespace Fusumity.Utility
 			return str.FromJson<T>();
 		}
 
-		public static void Save(string key, float value) =>
+		public static void Save(string key, float value, bool save = true)
+		{
 			PlayerPrefs.SetFloat(key, value);
+
+			if (save)
+				PlayerPrefs.Save();
+		}
 
 		public static float Load(string key, float defaultValue = default) =>
 			PlayerPrefs.GetFloat(key, defaultValue);
 
-		public static void Save(string key, int value) =>
+		public static void Save(string key, int value, bool save = true)
+		{
 			PlayerPrefs.SetInt(key, value);
+
+			if (save)
+				PlayerPrefs.Save();
+		}
 
 		public static int Load(string key, int defaultValue = default) =>
 			PlayerPrefs.GetInt(key, defaultValue);
